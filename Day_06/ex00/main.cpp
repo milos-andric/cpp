b62c9e86e670a4c5b85ea37df3aaa45cb5bf5a81
milos-andric <55629183+milos-andric@users.noreply.github.com>
2021-04-22 14:10:56 +0200

    ko

---
+++ Day_06/ex00/main.cpp
@@ -0,0 +1,137 @@
+# define ERR_ARG "please launch program with one number only, either a char, an integer or a decimal number (with or without an f after it).\nexamples :\n\t\t- 'c'\n\t\t- '42'\n\t\t- '56.5'\n\t\t- '59.7f'\n"
+
+# include <iostream>
+# include <iomanip>
+# include <string>
+# include <cmath>
+# include <cfloat>
+
+void    display_char(double num)
+{
+    char car = static_cast<char>(num);
+    
+    std::cout << "char =\t\t" ;
+
+    if (std::isnan(num) || std::isinf(num))
+        std::cout << "impossible" << std::endl;
+    else if (!isprint(car))   
+        std::cout << "Non displayable" << std::endl;
+    else
+        std::cout << car << std::endl;
+}
+
+void    display_int(double num)
+{
+    int car = static_cast<int>(num);
+    
+    std::cout << "int =\t\t" ;
+
+    if (std::isnan(num) || std::isinf(num))
+        std::cout << "impossible" << std::endl;
+    else
+        std::cout << car << std::endl;
+}
+
+void    display_float(double num)
+{
+    float car = static_cast<float>(num);
+    
+    std::cout << "float =\t\t" ;
+
+    std::cout << std::setprecision(7);
+    if ((car - static_cast<int>(car)) == 0)
+        std::cout << car << ".0f" << std::endl;
+    else
+        std::cout << car << "f" << std::endl;       
+}
+
+void    display_double(double num)
+{   
+    std::cout << "double =\t" ;
+
+    std::cout << std::setprecision(15);
+    if ((num - static_cast<int>(num)) == 0)
+        std::cout << num << ".0" << std::endl;
+    else
+        std::cout << num << std::endl;       
+}
+
+void    displayer(std::string s)
+{
+    double num = 0;
+
+    if ((s.size() == 1) && (isprint(s[0]) && (!(isdigit(s[0])))))
+        num = s[0];
+    else if (!(isprint(s[0])))
+    {
+        std::cout << "Give printable character at least..." << std::endl;
+        return;
+    }    
+    else
+        num = stod(s);
+    display_char(num);
+    display_int(num);
+    display_float(num);
+    display_double(num);
+}
+
+void    converter(std::string s)
+{
+    try
+    {
+        displayer(s);
+    }
+    catch(const std::exception& e)
+    {
+        converter("nan");
+    }
+}
+
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cerr << ERR_ARG << std::endl;
+        return(-1);
+    }
+    else
+    {
+        std::string s = argv[1];
+        converter(s);
+    }
+
+    return (0);
+}
+
+// void    test_conv(std::string s)
+// {
+//     std::cout << s << std::endl << std::endl;
+
+//     converter(s);
+//     std::cout << std::endl;
+// }
+
+// int main(void)
+// {
+//     test_conv("0");
+//     test_conv("42");
+//     test_conv("42.0");
+//     test_conv("42.042");
+//     test_conv("-42");
+//     test_conv("-42.042");
+//     test_conv("a");
+//     test_conv("A");
+//     test_conv("\n");
+//     test_conv("nan");
+//     test_conv("inf");
+//     test_conv("+inf");
+//     test_conv("-inf");
+//     test_conv("9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999");
+//     test_conv("askdjhvasd");
+//     test_conv("2030asdf305");
+//     test_conv("----25fsdf5");
+
+
+//     return(0);
+// }
